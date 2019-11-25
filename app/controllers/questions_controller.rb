1421b6dbf185a799f75dd9f896fdc3006b599c33
Pablo Maisuls <pablo_maisuls@live.com>
2019-11-25 14:42:18 +0100

    completed stupid coach challenge

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,9 +9,9 @@ class QuestionsController < ApplicationController
         @answer = 'Great!'
       elsif @question.include?('?')
        @answer = 'Silly question, get dressed and go to work!'
-    end
     else
       @answer = "I don't care, get dressed and go to work!"
+      end
     end
   end
 end
